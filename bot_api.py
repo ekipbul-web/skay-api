ff1e1c94b119e13bafb9e5c8bcbaf2934a7d9a4b
ekipbul-web <tunay1111115@gmail.com>
2026-06-27 18:53:01 +0400

    Create bot_api.py

---
+++ bot_api.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+import random
+import time
+import os
+
+app = Flask(__name__)
+CORS(app)
+pending_codes = {}
+BOT_TOKEN = '8875637835:AAFYihJubBsfNgvGo1p122a_NoXDsvyvU9Y'
+
+@app.route('/')
+def home():
+    return jsonify({'status': 'SKAY API Running', 'developer': '@TuncaySkay'})
+
+@app.route('/send_code', methods=['POST'])
+def send_code():
+    data = request.json
+    telegram = data.get('telegram', '').replace('@', '')
+    username = data.get('username', '')
+    if not telegram:
+        return jsonify({'success': False, 'message': 'Telegram gerekli'})
+    code = str(random.randint(100000, 999999))
+    try:
+        r = requests.get(f'https://api.telegram.org/bot{BOT_TOKEN}/getUpdates', timeout=5)
+        chat_id = None
+        if r.status_code == 200:
+            for u in reversed(r.json().get('result', [])):
+                msg = u.get('message', {})
+                if msg.get('from', {}).get('username', '').lower() == telegram.lower():
+                    chat_id = msg.get('chat', {}).get('id')
+                    break
+        if not chat_id:
+            return jsonify({'success': False, 'message': f'@{telegram} bota START vermemis!'})
+        r2 = requests.post(f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage', json={'chat_id': chat_id, 'text': f'SKAY API Kod: {code}'}, timeout=5)
+        if r2.status_code == 200:
+            pending_codes[username] = {'code': code, 'expires': time.time() + 300}
+            return jsonify({'success': True, 'message': 'Kod gonderildi!'})
+    except: pass
+    return jsonify({'success': False, 'message': 'Hata'})
+
+@app.route('/verify_code', methods=['POST'])
+def verify_code():
+    data = request.json
+    u, c = data.get('username', ''), data.get('code', '')
+    if u in pending_codes and pending_codes[u]['code'] == c and time.time() < pending_codes[u]['expires']:
+        del pending_codes[u]
+        return jsonify({'success': True, 'message': 'Dogrulandi!'})
+    return jsonify({'success': False, 'message': 'Gecersiz kod!'})
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
